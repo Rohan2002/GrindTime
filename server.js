8ddaf7d754474f15ca2e727f303767f297c73652
Rohan2002 <rohandeshpande832@gmail.com>
2020-02-07 23:02:00 -0500

    merge

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
+// const https = require('https');
+// const path = require('path');
+// const fs = require('fs');
 const app = express();
+const bodyParser = require("body-parser");
 const cors = require('cors');
 app.use(cors());
 
@@ -15,6 +16,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
+
+const router = express.Router();
+
+app.get('/findAPI', callD_alembert);
+
+function callD_alembert(req, res) {
+  // using spawn instead of exec, prefer a stream over a buffer
+  // to avoid maxBuffer issue
+  var spawn = require('child_process').spawn;
+  var process = spawn('python', ['./app.py',
+    req.query.funds, // starting funds
+    req.query.size, // (initial) wager size
+    req.query.count, // wager count — number of wagers per sim
+    req.query.sims // number of simulations
+  ]);
+  process.stdout.on('data', function (data) {
+    res.send(data.toString());
+  });
+}
 app.use("/api", router);
 
 app.listen(8080,()=>{
