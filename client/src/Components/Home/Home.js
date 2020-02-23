c4c615c0d4ec878f1897eaa99371cf60469275e6
Rohan2002 <rohandeshpande832@gmail.com>
2020-02-23 00:41:09 -0500

    added code

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,72 +1,81 @@
-import React from 'react';
-import Navbar from '../Navbar/Navbar';
-import Footer from '../Footer/Footer';
-import {Header} from 'semantic-ui-react';
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
+import { Header } from "semantic-ui-react";
 import axios from "axios";
-export default class Home extends React.Component
-{
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, long:null, humidity:null, temp: null, error: "" }
-      }
-  
-    componentDidMount() {
-      window.navigator.geolocation.getCurrentPosition(
-        position => {
-          this.setState({lat: position.coords.latitude });
-          this.setState({long: position.coords.longitude});
-          this.fetchAPI();
-        },
-        err => {
-          this.setState({ error: err.message });
-        }
-      );
-      
-    }
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat: null,
+      test: "hello",
+      long: null,
+      humidity: null,
+      temp: null,
+      error: "",
+      ML_result: "Loading..."
+    };
+  }
 
-    KtoC(temp)
-    {
-        return (temp - 273.15);
-    }
-    fetchAPI()
-    {
-        const pointer= this;
-        let API_CALL = fetch(`http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.long}&appid=7e792f31424709f0db3b655402a327bc`);
-        API_CALL.then(function(response) {
-            return response.json();
-          })
-            .then(function(data) {
-                pointer.setState({humidity: data["main"]["humidity"]})
-                let TEMP = Math.round(pointer.KtoC(parseFloat(data["main"]["temp"])));
-                pointer.setState({temp: TEMP});
-                
-                axios.post("http://localhost:3001/api/putData", {
-                  Lat: pointer.state.lat,
-                  Long: pointer.state.long,
-                  Humidity: data["main"]["humidity"],
-                  Temp: TEMP
-                })
-                .then(response => { 
-                  console.log(response)
-                })
-                .catch(error => {
-                    console.log(error.response)
-                });      
-            });
-    }
-    
-    render()
-    {
-        return(
-            <div className={"contain_all"}>
-                Lat: {this.state.lat}
-                <br/>
-                Long: {this.state.long}
-                <br/>
-                Humidity: {this.state.humidity}%
-                <br/>
-                Temperature: {this.state.temp} Degree Celcius
-            </div>
-        );
-    }
+  componentWillMount() {}
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+        this.setState({ long: position.coords.longitude });
+        this.fetchAPI();
+      },
+      err => {
+        this.setState({ error: err.message });
+      }
+    );
+  }
+
+  KtoC(temp) {
+    return temp - 273.15;
+  }
+  fetchAPI() {
+    const pointer = this;
+    let API_CALL = fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${this.state.lat}&lon=${this.state.long}&appid=7e792f31424709f0db3b655402a327bc`
+    );
+    API_CALL.then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      pointer.setState({ humidity: data["main"]["humidity"] });
+      let TEMP = Math.round(pointer.KtoC(parseFloat(data["main"]["temp"])));
+      pointer.setState({ temp: TEMP });
+
+      axios
+        .post("http://localhost:8080/api/getdata", {
+          Lat: pointer.state.lat,
+          Long: pointer.state.long,
+          Humidity: data["main"]["humidity"],
+          Temp: TEMP
+        })
+        .then(function(response) {
+          return response.data;
+        })
+        .then(function(data) {
+          console.log(data)
+          data===1 ? pointer.setState({ML_result: "Good Time to Study"}) : pointer.setState({ML_result: "Bad Time to Study"})
+        });
+    });
+  }
+
+  render() {
+    return (
+      <div className={"contain_all"}>
+        Lat: {this.state.lat}
+        <br />
+        Long: {this.state.long}
+        <br />
+        Humidity: {this.state.humidity}%
+        <br />
+        Temperature: {this.state.temp} Degree Celcius
+        <br />
+        ML Result: {this.state.ML_result}
+      </div>
+    );
+  }
 }
--- server.js
+++ server.js
@@ -1,24 +1,31 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+//bodyparser
 const bodyParser = require("body-parser");
 const logger = require("morgan");
+
+//Api router
 const router = express.Router();
-const port = process.env.PORT || 3001;
-
-//app.use(cors());
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(logger("dev"));
 
-router.post("/putData", (req, res) => {
-  const { Lat, Long, Humidity, Temp } = req.body;
-  console.log("Rohan");
-  console.log(Lat);
-  return "Rohan";
+router.post("/getdata", function(req, res) {
+  var Humidity = req.body.Humidity;
+  var Lat = req.body.Lat;
+  var Long = req.body.Long;
+  var Temp = req.body.Temp;
+  var spawn = require("child_process").spawn;
+  var process = spawn("python3", ["./app.py", Temp, Humidity, Lat, Long]);
+  process.stdout.on("data", function(data) {
+    res.status(200).send(data.toString());
+  });
 });
+
+
 app.use("/api", router);
-app.listen(port, function() {
-  console.log("Runnning on " + port);
+app.listen(8080, function() {
+  console.log("server: port 8080");
 });
